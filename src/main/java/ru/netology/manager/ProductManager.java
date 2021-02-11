65f2f53599ccee6dbe2becbc38092cec90ffc01f
boog25 <voroshilov.d.v123@gmail.com>
2021-02-11 20:29:43 +0500

    add files

---
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -0,0 +1,52 @@
+package ru.netology.manager;
+import ru.netology.domain.Product;
+import ru.netology.repository.ProductRepository;
+
+public class ProductManager {
+    private ProductRepository repository;
+
+    public ProductManager(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+
+    public void add(Product item) {
+        repository.save(item);
+    }
+
+    public Product[] getAll() {
+        Product[] items = repository.findAll();
+        Product[] result = new Product[items.length];
+        for (int i = 0; i < result.length; i++) {
+            int index = items.length - i - 1;
+            result[i] = items[index];
+        }
+        return result;
+    }
+
+    public void removeById(int id) {
+        repository.removeById(id);
+    }
+
+
+    public Product[] findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(int id) {
+        return repository.findById(id);
+    }
+
+    public Product[] searchBy(String name) {
+        Product[] result = new Product[0];
+        for (Product product : repository.findAll()) {
+            if (product.matches(name)) {
+                Product[] tmp = new Product[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = product;
+                result = tmp;
+            }
+        }
+        return result;
+    }
+}
